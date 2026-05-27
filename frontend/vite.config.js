70eb28cefa7ee75221157058f706207a3a0c203a
2004kmsung <2004kmsung@gmail.com>
2026-05-28 02:01:11 +0900

    fix8

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-
-    allowedHosts: 'all',
+    allowedHosts: 'all', 
     preview: {
-      allowedHosts: 'all'
+      allowedHosts: 'all' 
     }
   }
 })
